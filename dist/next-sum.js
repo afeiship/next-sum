274477c965c6b101c78210d2b5677e869560f6f6
afeiship <1290657123@qq.com>
2017-09-21 09:53:35 +0800

    isseu: build

---
+++ dist/next-sum.js
@@ -0,0 +1,33 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+  var ARRAY_PROTO = Array.prototype;
+
+  nx.sum = function (inTarget) {
+    var arg1 = arguments[1];
+    var arg2 = arguments[2];
+    var values = [];
+
+    nx.each(inTarget, function (key, value) {
+      if (arg1) {
+        if (arg1.call(inTarget, key, value)) {
+          values.push(value);
+        }
+      } else {
+        values.push(value);
+      }
+    });
+
+    return values.reduce(function (current, next) {
+      return arg2
+        ? arg2.call(values, current, next)
+        : (current + next);
+    });
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.sum;
+  }
+
+}());
